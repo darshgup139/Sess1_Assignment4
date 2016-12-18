5f4b6c19eb0d3c2b6042f9038ce73dc822531bc1
Adarsh Gupta <adarshgupta139@gmail.com>
2016-12-18 17:17:03 +0530

    Added Source Code

---
+++ Assignment 1.4/src/Sess1_Assigmnt4.java
@@ -0,0 +1,45 @@
+
+public class Sess1_Assigmnt4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int start=65,end=90,i;
+		char c;
+		for(i=start;i<=end;i++)
+		{
+			c=(char) i;
+			System.out.println(i+"-"+c);	
+		}
+	}
+
+}
+/*
+******************************************************************************
+65-A
+66-B
+67-C
+68-D
+69-E
+70-F
+71-G
+72-H
+73-I
+74-J
+75-K
+76-L
+77-M
+78-N
+79-O
+80-P
+81-Q
+82-R
+83-S
+84-T
+85-U
+86-V
+87-W
+88-X
+89-Y
+90-Z
+******************************************************************************
+*/
